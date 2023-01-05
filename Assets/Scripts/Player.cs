0fb21f0969652de5135cd2ef8699d2c6b69b3b6b
Bagus Wicaksono <bagusbwicaksono5@student.uns.ac.id>
2023-01-05 19:12:08 +0700

    Final game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float jumpForce = 7f;
+
+	public Rigidbody2D rb;
+	public SpriteRenderer sr;
+
+	public string currentColor;
+
+	public Color colorCyan;
+	public Color colorYellow;
+	public Color colorMagenta;
+	public Color colorPink;
+
+	void Start ()
+	{
+		SetRandomColor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+		{
+			rb.velocity = Vector2.up * jumpForce; //velocity kecepatan 
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "ColorChanger")
+		{
+			SetRandomColor();
+			Destroy(col.gameObject);
+			return;
+		}
+
+		if (col.tag != currentColor)
+		{
+			SceneManager.LoadScene("End Color");
+		}
+	}
+
+	void SetRandomColor ()
+	{
+		int index = Random.Range(0, 4);
+
+		switch (index)
+		{
+			case 0:
+				currentColor = "Cyan";
+				sr.color = colorCyan;
+				break;
+			case 1:
+				currentColor = "Yellow";
+				sr.color = colorYellow;
+				break;
+			case 2:
+				currentColor = "Magenta";
+				sr.color = colorMagenta;
+				break;
+			case 3:
+				currentColor = "Pink";
+				sr.color = colorPink;
+				break;
+		}
+	}
+}
